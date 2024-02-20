bb2083a140c8151a04d51f127d40fc17c5c2eac4
Huy Nguyen <huy.h.nguy@gmail.com>
2024-02-20 14:54:27 -0600

    increase and decrease buttons will change the quantity of item desired for user

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,26 @@
 import decrease from "../../images/decrease.svg";
 import increase from "../../images/increase.svg";
 import styles from "./Product.module.css"
+import { useState } from "react";
 
 export default function Product({ data }) {
+    const [quantity, setQuantity] = useState(0);
+    
+    function handleIncrease() {
+        setQuantity(quantity + 1);
+    }
+
+    function handleDecrease() {
+        setQuantity(quantity - 1);
+    }
+
     return (
-        <div>
-            <img src={data.image}></img>
-            <h1>{data.title}</h1>
-            <img src={decrease} className={styles.quantity}></img>
-            <input type="number"></input>
-            <img src={increase} className={styles.quantity}></img>
+        <div className={styles.container}>
+            <img src={data.image} className={styles.image}></img>
+            <h1 className={styles.title}>{data.title}</h1>
+            <img src={decrease} className={styles.quantity} onClick={handleDecrease}></img>
+            <input type="number" className={styles.number} min={0} max={10} value={quantity}></input>
+            <img src={increase} className={styles.quantity} onClick={handleIncrease}></img>
             <button>Add to cart</button>
         </div>
     )
