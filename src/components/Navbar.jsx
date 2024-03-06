9518c3691277540d835f7b6df1de05796ff47454
Huy Nguyen <huy.h.nguy@gmail.com>
2024-03-06 15:19:11 -0600

    Within the navigation bar, shopping cart will show total quantity of items in cart regardless of what tab user is on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
-function Tab({ tabname, path, data }) {
+function Tab({ tabname, path }) {
     return (
         <div>
-            <Link 
-                to={path}
-                state= {data}
-            >{tabname}</Link>
+            <Link to={path}>{tabname}</Link>
         </div>
     )
 };
 
-export default function Navbar(data) {
+function CartTab({ tabname, path }) {
+    let totalQuantity = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+        const itemQuantity = Number(localStorage[i].substring(localStorage[i].indexOf('-') + 1));
+        totalQuantity = totalQuantity + itemQuantity;
+    }
+
+    return (
+        <div>
+            <Link to={path}>{tabname} ({totalQuantity})</Link>
+        </div>
+    )
+}
+
+export default function Navbar() {
+
     return (
         <div>
             <Tab tabname= "Home" path= "/"/>
             <Tab tabname= "Shop" path= "/shop"/>
-            <Tab tabname= "Shopping Cart" path= "/cart" data={data}/>
+            <CartTab tabname= "Shopping Cart" path= "/cart"/>
         </div>
     )
 }
