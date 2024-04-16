3bedd3cdb7b94097af18f69f5a349a119856add7
Huy Nguyen <huy.h.nguy@gmail.com>
2024-04-16 15:12:35 -0500

    add stock photo to homepage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../Navbar/Navbar"
 import styles from "./App.module.css"
+import ground from "../../images/clothing-on-ground.jpg"
 
 function App() {
   return (
     <>
       <p className={styles.title}>HUYS</p>
       <Navbar activeTab='home'/>
-      <h1 className={styles.about}>Welcome to the greatest online store known to mankind. Decades, centuries from now people will remember Huys. It will be written in history books because of how much better the quality of the products are in comparison to anything else out there. Browse our store in awe and remember it because you are a part of history in the making; the history of Huys.</h1>
+      <img src={ground} className={styles.ground}></img>
+      <p className={styles.credit}>Photo by Lum3n via <a href="https://www.pexels.com/photo/low-angle-view-of-shoes-322207/" className={styles.link}>Pexels</a></p>
     </>
   )
 }
