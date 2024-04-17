fb110b328813c87b37721a26582f653027f2d3ad
Huy Nguyen <huy.h.nguy@gmail.com>
2024-04-17 16:29:12 -0500

    logo sizing adjusted

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,9 +2,6 @@ import styles from "./Logo.module.css"
 import { Link } from "react-router-dom";
 
 export default function Logo() {
-    function handleClick() {
-
-    }
     return (
         <Link to='/' className={styles.title}>HUYS</Link>
     )
