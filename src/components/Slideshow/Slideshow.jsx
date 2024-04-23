1b436d1471fa0a349cf69a26ff1845fdf4377f28
Huy Nguyen <huy.h.nguy@gmail.com>
2024-04-23 17:35:16 -0500

    fix bug that wouldnt apply transitions to any images past the second image in the slider.
    Keys were given to each image component that would make react rerender new images.

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -4,11 +4,12 @@ import right from "../../images/right.svg"
 import left from "../../images/left.svg"
 import { useState } from "react"
 import one from "../../images/slideshow-1.jpg"
+import two from "../../images/slideshow-2.jpg"
+import three from "../../images/slideshow-3.jpg"
 
 export default function Slideshow() {
     const [activePhoto, setActivePhoto] = useState(0);
-
-    const imageArray = [clothing, one];
+    const imageArray = [clothing, one, two, three];
 
     function handleClick() {
         setActivePhoto(activePhoto + 1)
@@ -18,10 +19,10 @@ export default function Slideshow() {
         <div className={styles.slideshow}>
             {activePhoto > 0 ? 
             <>
-                <img src={imageArray[activePhoto - 1]} className={`${styles.clothing} ${styles.fadeOut}`}></img>
-                <img src={imageArray[activePhoto]} className={`${styles.clothing} ${styles.second}`}></img>
+                <img key={activePhoto - 1} src={imageArray[activePhoto - 1]} className={`${styles.photo} ${styles.fadeOut}`}></img>
+                <img key={activePhoto} src={imageArray[activePhoto]} className={`${styles.photo} ${styles.newPhoto}`}></img>
             </> : 
-                <img src={imageArray[0]} className={styles.clothing}></img> }
+                <img src={imageArray[0]} className={styles.photo}></img> }
             <img src={right} className={styles.right} onClick={handleClick}></img>
             <img src={left} className={styles.left}></img>
         </div>
