b46d2f64b45f537439c38aa0cef0cd79cd77f162
Huy Nguyen <huy.h.nguy@gmail.com>
2024-04-23 16:35:45 -0500

    when user clicks right arrow of image slider, current image fades out and next image fades in

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -2,12 +2,27 @@ import clothing from "../../images/clothing-on-ground.jpg"
 import styles from "./Slideshow.module.css"
 import right from "../../images/right.svg"
 import left from "../../images/left.svg"
+import { useState } from "react"
+import one from "../../images/slideshow-1.jpg"
 
 export default function Slideshow() {
+    const [activePhoto, setActivePhoto] = useState(0);
+
+    const imageArray = [clothing, one];
+
+    function handleClick() {
+        setActivePhoto(activePhoto + 1)
+    }
+
     return(
         <div className={styles.slideshow}>
-            <img src={clothing} className={styles.clothing}></img>
-            <img src={right} className={styles.right}></img>
+            {activePhoto > 0 ? 
+            <>
+                <img src={imageArray[activePhoto - 1]} className={`${styles.clothing} ${styles.fadeOut}`}></img>
+                <img src={imageArray[activePhoto]} className={`${styles.clothing} ${styles.second}`}></img>
+            </> : 
+                <img src={imageArray[0]} className={styles.clothing}></img> }
+            <img src={right} className={styles.right} onClick={handleClick}></img>
             <img src={left} className={styles.left}></img>
         </div>
     )
