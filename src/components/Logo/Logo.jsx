33eb2b844b2cf22e5bb4fe473e4c653844f20d7b
Huy Nguyen <huy.h.nguy@gmail.com>
2024-05-01 21:02:40 -0500

    fix bug in which user could click outside logo and still click it.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,16 @@
 import styles from "./Logo.module.css"
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Logo() {
+    let navigate = useNavigate(); 
+
+    function handleClick() {
+      let path = `/`; 
+      navigate(path);
+    }
+
     return (
-        <Link to='/' className={styles.title}>HUY'S</Link>
+        <h1 onClick={handleClick} className={styles.title}>HUY'S</h1>
     )
 }
